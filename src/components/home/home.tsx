b2a484ac56fade1e6e6d033e75426cf1a46334a9
CookieeGuy <142661309+CookieeGuy@users.noreply.github.com>
2024-04-20 14:40:02 +0330

    pricings

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,17 +1,20 @@
 import classes from "./home.module.scss";
 import { Header } from "@/components/header/header";
 import { Footer } from "@/components/footer/footer";
+import ProLeagueInfo from "./pro-league-info/proLeagueInfo";
+import Pricings from "./pricings/pricings";
 
 export const HomeComponent: React.FC = () => {
-  return(
+  return (
     <div className={classes.home}>
       <Header />
       <main className={classes.mainWrapper}>
         <div className={classes.main}>
-
+          <ProLeagueInfo />
+          <Pricings />
         </div>
       </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
---
+++ src/components/home/pricings/pricing-card/pricingCard.tsx
@@ -0,0 +1,46 @@
+import Image, { StaticImageData } from "next/image";
+import classes from "./pricingCard.module.scss";
+
+export interface PricingCardProps {
+  image: string | StaticImageData;
+  logo: React.ReactElement;
+  description: string;
+  reward: number | string;
+  entrance: number | string;
+}
+
+const PricingCard: React.FC<PricingCardProps> = ({
+  image,
+  logo,
+  description,
+  reward,
+  entrance,
+}) => {
+  return (
+    <div className={classes.pricingCard}>
+      <Image className={classes.background} src={image} alt="Card Background" />
+      <div className={classes.content}>
+        <div className={classes.logo}>{logo}</div>
+        <div className={classes.info}>
+          <p className={classes.description}>{description}</p>
+          <div className={classes.reward}>
+            <p className={classes.title}>Reward</p>
+            <p className={classes.value}>
+              {reward === "No Reward" ? "" : <span>&#8364;</span>}
+              {reward.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            </p>
+          </div>
+          <div className={classes.entrance}>
+            <p className={classes.title}>Entrance</p>
+            <p className={classes.value}>
+              {entrance === "FREE" ? "" : <span>&#8364;</span>}
+              {entrance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PricingCard;
